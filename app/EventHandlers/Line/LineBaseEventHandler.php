6a84ee74ad2e6c4eb8fd73df54e2b40da098aa3c
teraiken <terai.ken@relation2012.com>
2024-03-10 19:56:41 +0900

    line base event handler

---
+++ app/EventHandlers/Line/LineBaseEventHandler.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\EventHandlers\Line;
+
+use App\EventHandlers\EventHandler;
+use Illuminate\Support\Facades\Log;
+use LINE\Clients\MessagingApi\Api\MessagingApiApi;
+use LINE\Clients\MessagingApi\ApiException;
+use LINE\Clients\MessagingApi\Model\Message;
+use LINE\Clients\MessagingApi\Model\ReplyMessageRequest;
+use LINE\Clients\MessagingApi\Model\TextMessage;
+use LINE\Constants\MessageType;
+use LINE\Webhook\Model\Event;
+
+abstract class LineBaseEventHandler implements EventHandler
+{
+    /**
+     * @param MessagingApiApi $bot
+     * @param Event $event
+     */
+    public function __construct(
+        private MessagingApiApi $bot,
+        private Event $event
+    ) {
+    }
+
+    /**
+     * @param string $replyToken
+     * @param string $text
+     * @return void
+     */
+    protected function replyText(string $replyToken, string $text): void
+    {
+        $textMessage = (new TextMessage(['text' => $text, 'type' => MessageType::TEXT]));
+
+        return $this->replyMessage($replyToken, $textMessage);
+    }
+
+    /**
+     * @param string $replyToken
+     * @param Message $message
+     * @return void
+     */
+    protected function replyMessage(string $replyToken, Message $message): void
+    {
+        $request = new ReplyMessageRequest([
+            'replyToken' => $replyToken,
+            'messages' => [$message],
+        ]);
+
+        try {
+            $this->bot->replyMessage($request);
+        } catch (ApiException $e) {
+            Log::error('BODY:' . $e->getResponseBody());
+            throw $e;
+        }
+    }
+}
